e11a5147df72fd8f3720e5824b38dd613cd4e746
Dallin Bunnell <dallinbunnellswims@hotmail.com>
2015-11-17 10:34:28 -0700

    Class setup
    I made sure that my class for LinkedList has all of the appropriate
    syntax for its implementation

--- GuiWithLinkedLists/LinkedList.cpp
+++ GuiWithLinkedLists/LinkedList.cpp
@@ -5,3 +5,17 @@
 LinkedList::LinkedList()
 {
 }
+LinkedList::~LinkedList()
+{}
+void LinkedList::add(std::string value)
+{}
+void LinkedList::insert(int pos, std::string value)
+{}
+void LinkedList::remove(int pos)
+{}
+void LinkedList::set(int pos, double value)
+{}
+int LinkedList::size()
+{}
+std::string  LinkedList::get(int pos)
+{}
--- GuiWithLinkedLists/LinkedList.h
+++ GuiWithLinkedLists/LinkedList.h
@@ -1,11 +1,22 @@
 #pragma once
 #include <iostream>
-
-ref class LinkedList
+struct PictureNode {
+	std::string picturePath;
+	PictureNode *next;
+	PictureNode *prev;
+};
+class LinkedList
 {
+private:
+	PictureNode *head;
+	PictureNode *tail;
 public:
 	LinkedList();
-	LinkedList *next
-
+	~LinkedList();
+	void add(std::string value); // Adds to the end of the list
+	void insert(int pos, std::string value); // Inserts the value at the given index
+	void remove(int pos); // Removes element at given pos
+	std::string get(int pos); // Returns element at that pos
+	void set(int pos, double value); // Sets a position in list
+	int size(); // The number of elements stored in the list
 };
-
